0c7dd0e898ffc3ca68bdeb21c1a3f80cad78e0d0
Lucas Mocroski <devmocroski@gmail.com>
2022-12-04 20:36:15 -0300

    Criando o Repository com Spring Data

--- src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
@@ -1,11 +1,15 @@
 package br.com.springboot.curso_springboot.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.springboot.curso_springboot.model.Usuario;
+import br.com.springboot.curso_springboot.repisitory.UsuarioRepository;
 
 /**
  *
@@ -14,11 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController  //intercepta todas as requisicoes que forem feito o mapeamento
 public class GreetingsController {
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
+	
+	@Autowired //injecao de dependencia ou CD/CDI
+	private UsuarioRepository usuarioRepository;
+    
+   
     @RequestMapping(value = "/{name}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String greetingText(@PathVariable String name) {
@@ -28,6 +32,10 @@ public class GreetingsController {
     @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
     @ResponseStatus(HttpStatus.OK)
     public String retornaOlaMundo(@PathVariable String nome) {
+    	
+    	Usuario usuario = new Usuario();
+    	usuario.setNome(nome);
+    	usuarioRepository.save(usuario); //grava no banco de dados
     	
     	return "Ola Mundo " + nome;
     	
