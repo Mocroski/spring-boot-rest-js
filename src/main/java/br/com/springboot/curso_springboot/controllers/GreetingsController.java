dfa835d47ae142944cd0014bd6e6a0b53af39d0b
Lucas Mocroski <devmocroski@gmail.com>
2022-12-05 11:22:40 -0300

    Criando metodo para deletar usuario do banco

--- src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
@@ -3,12 +3,14 @@ package br.com.springboot.curso_springboot.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,6 +65,16 @@ public class GreetingsController {
     	Usuario user = usuarioRepository.save(usuario);
     	
     	return new ResponseEntity<Usuario>(user, HttpStatus.CREATED);
+    	
+    }
+    
+    @DeleteMapping(value = "delete") //mapea a url
+    @ResponseBody
+    public ResponseEntity<String> delete(@RequestParam Long idUser) { //recebe os dados para salvar
+    	
+    	usuarioRepository.deleteById(idUser);//funcao do spring data para deletar do banco faz tudo sozinho
+    	
+    	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK); //retorna a reposta 
     	
     }
     
