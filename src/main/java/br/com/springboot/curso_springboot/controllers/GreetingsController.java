f9f53f3039a74ad08a2155acb04f347b97b38f9e
Lucas Mocroski <devmocroski@gmail.com>
2022-12-05 11:49:17 -0300

    Criado metodo para buscar por ID e atualizar no banco

--- src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
+++ src/main/java/br/com/springboot/curso_springboot/controllers/GreetingsController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -75,6 +76,30 @@ public class GreetingsController {
     	usuarioRepository.deleteById(idUser);//funcao do spring data para deletar do banco faz tudo sozinho
     	
     	return new ResponseEntity<String>("User deletado com sucesso", HttpStatus.OK); //retorna a reposta 
+    	
+    }
+    
+    @GetMapping(value = "buscaruserid") //mapea a url
+    @ResponseBody
+    public ResponseEntity<Usuario> buscarUserId(@RequestParam(name="idUser") Long idUser) { //recebe os dados
+    	
+    	Usuario usuario = usuarioRepository.findById(idUser).get();
+    	
+    	return new ResponseEntity<Usuario>(usuario, HttpStatus.OK);
+    	
+    }
+    
+    @PutMapping(value = "atualizar") //mapea a url
+    @ResponseBody
+    public ResponseEntity<?> atualizar(@RequestBody Usuario usuario) { //recebe os dados para salvar
+    	
+    	if(usuario.getId() == 0) {
+    		return new ResponseEntity<String>("Id nao informado", HttpStatus.OK);
+    	}
+    	
+    	Usuario user = usuarioRepository.saveAndFlush(usuario);
+    	
+    	return new ResponseEntity<Usuario>(user, HttpStatus.OK);
     	
     }
     
